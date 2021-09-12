ed53bb22d3e5890791153ba5b0f5eb9658205a72
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-12 21:33:01 +0900

    [Feature] - Normal Function style

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var app = express();
 
 console.log("Hi Index");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+function getHomeURL(req, res){
+  res.render("index", {title: "Express"});
+}
+
+router.get("/", getHomeURL);
+
+// function handleListening(port){
+//   console.log("Listening on : http://localhost:${port}");
+// }
+
+
 
 module.exports = router;
